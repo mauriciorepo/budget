9bd688d2c65aafa0f973b006e4b2636d12f26302
mauriciorepo <mauriciovalencas@gmail.com>
2020-10-02 11:58:13 -0300

    configure ModelMapper

--- src/main/java/com/finance/budget/BudgetApplication.java
+++ src/main/java/com/finance/budget/BudgetApplication.java
@@ -1,10 +1,19 @@
 package com.finance.budget;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BudgetApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetApplication.class, args);
