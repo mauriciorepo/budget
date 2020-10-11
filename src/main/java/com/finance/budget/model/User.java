73451a6fffc0f6e8c7f924cefb021dbd4d798a66
mauriciorepo <mauriciovalencas@gmail.com>
2020-10-10 23:29:46 -0300

    Building User as Entity annotation

--- src/main/java/com/finance/budget/model/User.java
+++ src/main/java/com/finance/budget/model/User.java
@@ -1,16 +1,25 @@
 package com.finance.budget.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Data
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String login;
